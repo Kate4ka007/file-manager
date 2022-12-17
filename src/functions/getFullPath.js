1f445907278563680bc854df25aa9d4dff8acccb
Kate4ka007 <shlemena@mail.ru>
2022-12-17 15:02:37 +0300

    fix: change path

--- src/functions/getFullPath.js
+++ src/functions/getFullPath.js
@@ -3,7 +3,11 @@ import process from 'process';
 
 export const getFullPathToFile = (data) => {
   const path_to_file = data.split(' ')[1];
-  const current_path = process.cwd();
-  const full_path_to_file = path.join(current_path, path_to_file);
-  return full_path_to_file;
+  if (path.isAbsolute(path_to_file)) {
+    return path_to_file;
+  } else {
+    const current_path = process.cwd();
+    const full_path_to_file = path.join(current_path, path_to_file);
+    return full_path_to_file;
+  }
 };
